8f37104386c5ffb9f14edc0386f48d8b3f5832b6
Erlend Dahlen <37074633+erlenddahlen@users.noreply.github.com>
2020-11-12 13:09:22 +0100

    Add files via upload

--- LOS.m
+++ LOS.m
@@ -1,6 +1,6 @@
-function X_d = LOS(x, y, x1, y1, x2, y2, K_p)
-
+function X_d = LOS(x, y, x1, y1, x2, y2, K_p, Bc)
+Bc = Bc*(pi/180);
 [y_e, pi_p] = ctrackW(x2, y2, x1, y1, x, y);
 
-X_d = pi_p - atan(K_p*y_e);
+X_d = pi_p - atan(K_p*y_e)-Bc;
 
